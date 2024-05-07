16f3c1feab7f9288acdfc9401784dbf1c5f7b21a
pauulaferreiro <paulaferreiro09@gmail.com>
2024-05-07 14:08:00 +0200

    creación migraciones (comandos)

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ const url = "sqlite:blog.sqlite"; //nombre base de datos (blog.sqlite)
 
 const sequelize = new Sequelize(url, { logging: false }); //crear base de datos. logging false para limpiar salida terminal
 
+//"requerimientos"
 const Post = require('./post')(sequelize, Sequelize.DataTypes);
 const Attachment = require('./attachment')(sequelize, Sequelize.DataTypes);
 
