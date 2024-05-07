bb055cae5f2f2bfc489c16a4856406f0413147d7
pauulaferreiro <paulaferreiro09@gmail.com>
2024-05-07 11:52:03 +0200

    añadir express-partials

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var partials = require('express-partials');  
 
 var indexRouter = require('./routes/index');
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());   
 
 app.use('/', indexRouter);
 
