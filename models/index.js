1168dd0eae24851948f8d038d728d9c921e5c94a
pauulaferreiro <paulaferreiro09@gmail.com>
2024-05-07 13:52:06 +0200

    relaciones entre post y attachment

--- models/index.js
+++ models/index.js
@@ -4,4 +4,10 @@ const url = "sqlite:blog.sqlite"; //nombre base de datos (blog.sqlite)
 
 const sequelize = new Sequelize(url, { logging: false }); //crear base de datos. logging false para limpiar salida terminal
 
+const Post = require('./post')(sequelize, Sequelize.DataTypes);
+const Attachment = require('./attachment')(sequelize, Sequelize.DataTypes);
+
+Attachment.hasOne(Post, {as: 'post', foreignKey: 'attachmentId'});
+Post.belongsTo(Attachment, {as: 'attachment', foreignKey: 'attachmentId'});
+
 module.exports = sequelize;
