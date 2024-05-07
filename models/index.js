07d765589f93adae85d6c9ec7563c55ae9b3fd86
pauulaferreiro <paulaferreiro09@gmail.com>
2024-05-07 13:13:45 +0200

    comentarios en idex.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize'); //importar sequelize 
 
-const url = "sqlite:blog.sqlite";
+const url = "sqlite:blog.sqlite"; //nombre base de datos (blog.sqlite)
 
-const sequelize = new Sequelize(url, { logging: false }); //crear base de datos
+const sequelize = new Sequelize(url, { logging: false }); //crear base de datos. logging false para limpiar salida terminal
 
 module.exports = sequelize;
