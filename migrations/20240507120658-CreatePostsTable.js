697ab70c29ef0913025070f4f4591c7ee891180f
pauulaferreiro <paulaferreiro09@gmail.com>
2024-05-07 14:23:02 +0200

    migración posts

--- migrations/20240507120658-CreatePostsTable.js
+++ migrations/20240507120658-CreatePostsTable.js
@@ -3,20 +3,52 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable(
+      'Posts',
+      {
+          id: {
+              type: Sequelize.INTEGER, 
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true,
+              unique: true
+          },
+
+          title: {
+              type: Sequelize.STRING,
+              validate: {notEmpty: {msg: "Title no puede estar vacío"}}
+          },
+
+          body: {
+              type: Sequelize.TEXT,
+              validate: {notEmpty: {msg: "Body no puede estar vacío"}}
+          },
+
+          attachmentId: {
+            type: Sequelize.STRING,
+            references: {
+              model: "Attachments",
+              key: "id"
+            },
+
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+          },
+
+          createdAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          },
+
+          updatedAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          }
+      }, {  sync: {force: true} } //sincronizar si algo se modifica
+    );
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Posts');
   }
 };
